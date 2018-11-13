5617565938beda05f0f1cda678835e2b0dadc40e
DannyB <dannyb@dudamobile.com>
2018-11-13 19:29:30 +0200

    initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith({
+        SpringExtension.class
+})
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
